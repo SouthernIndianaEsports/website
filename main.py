a279e4d2dc4e28d6bebd4b01170a9b541610163b
endoffile78 <endoffile78@yahoo.com>
2019-03-19 15:21:19 -0500

    Use bootstrap 4 instead of bootstrap 3

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
-from navigation import navigation_init
-
 app = Flask(__name__)
 Bootstrap(app)
-
-navigation_init(app)
 
 @app.route("/")
 def index():
